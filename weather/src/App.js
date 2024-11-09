f30190fb69ba5e236cbaa37f71842c1e3fba2fbb
Cate Milan <catherine.milanetti@gmail.com>
2024-11-09 14:27:32 -0500

    added text

--- weather/src/App.js
+++ weather/src/App.js
@@ -3,7 +3,23 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">Weather App</div>);
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.shecodes.io/graduates/98761-catherine-milanetti"
+          target="_blank"
+        >
+          Cate Milan
+        </a>
+        and is an open-source on
+        <a
+          href="https://github.com/CxMilan/React-Weather-App-Week5"
+          target="_blank">
+          github
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
