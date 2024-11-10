84ab379d30f43f7b0264877bce923ff035e23600
Cate Milan <catherine.milanetti@gmail.com>
2024-11-09 20:50:00 -0500

    updated

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,33 +1,39 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
- export default function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.shecodes.io/graduates/98761-catherine-milanetti"
-          target="_blank"
-        >
-          Cate Milan
-        </a>
-        and is an open-source on
-        <a
-          href="https://github.com/CxMilan/React-Weather-App-Week5"
-          target="_blank"
-        >
-          GitHub
-        </a>
-        and hosted on
-        <a
-          href="https://app.netlify.com/sites/react-meteo-app-week5/overview"
-          target="_blank"
-        >
-          Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <Weather defaultCity="New York" />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/98761-catherine-milanetti"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Cate Milan
+          </a>
+          and is an open-source on
+          <a
+            href="https://github.com/CxMilan/React-Weather-App-Week5"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          and hosted on
+          <a
+            href="https://app.netlify.com/sites/react-meteo-app-week5/overview"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
